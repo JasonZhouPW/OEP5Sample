5b4f4e62fc7c1846d5e52953a913ace82e414356
easonzhouyc <43459775+easonzhouyc@users.noreply.github.com>
2018-09-26 11:10:59 +0800

    add withdraw method

--- OEP5Sample.py
+++ OEP5Sample.py
@@ -1,10 +1,14 @@
+'''
+A sample of OEP5 smart contract
+'''
 from boa.interop.System.Storage import GetContext, Get, Put, Delete
 from boa.interop.System.Runtime import CheckWitness, GetTime, Notify, Serialize, Log
 from boa.interop.System.ExecutionEngine import GetExecutingScriptHash
 from boa.interop.Ontology.Native import Invoke
 from boa.builtins import *
 
-owner = ToScriptHash('Ad4pjz2bqep4RhQrUAzMuZJkBC3qJ1tZuT')
+#modify to the admin address
+owner = ToScriptHash('XXXXXXXXXXXXXXXXXX')
 
 NAME = 'OEP5 Asset'
 SYMBOL = 'Asset Symbol'
@@ -16,7 +20,7 @@ INITED = 'Initialized'
 ASSET_INDEX_PREFIX = 'Index'
 TOTAL_ASSET_COUNT = 'TotalCount'
 ASSET_ID_PREFIX = 'AssetID'
-
+ONT_BALANCE_PREFIX = 'OntBalance'
 
 ctx = GetContext()
 
@@ -87,11 +91,20 @@ def Main(operation, args):
         return queryAssetByID(assetID)
     if operation == 'queryAssetCount':
         return queryAssetCount()
+    if operation == 'withdraw':
+        if len(args) != 1:
+            return False
+        account = args[0]
+        return withdraw(account)
 
     return False
 
 
 def init():
+    '''
+    initialize the assets
+    :return:
+    '''
     if not Get(ctx, INITED):
         Put(ctx, INITED, 'TRUE')
 
@@ -120,24 +133,48 @@ def init():
 
 
 def name():
+    '''
+    Name of the asset
+    :return:
+    '''
     return NAME
 
 
 def symbol():
+    '''
+    symbol of the asset
+    :return:
+    '''
     return SYMBOL
 
 
 def balanceOf(owner):
+    '''
+    balance of owned the assets
+    :param owner:
+    :return:
+    '''
     key = concatkey(OWNER_BALANCE_PREFIX, owner)
     return Get(ctx, key)
 
 
 def ownerOf(assetID):
+    '''
+    owner of the asset
+    :param assetID:
+    :return:
+    '''
     key = concatkey(OWNER_OF_ASSET_PREFIX, assetID)
     return Get(ctx, key)
 
 
 def transfer(toAcct, assetID):
+    '''
+    transfer the asset to another address
+    :param toAcct:
+    :param assetID:
+    :return:
+    '''
     if checkToAcctAndAsset(toAcct, assetID) == False:
         return False
 
@@ -151,6 +188,11 @@ def transfer(toAcct, assetID):
 
 
 def transferMulti(args):
+    '''
+    multi transfer
+    :param args:
+    :return:
+    '''
     for p in args:
         if len(p) != 2:
             return False
@@ -160,6 +202,12 @@ def transferMulti(args):
 
 
 def approve(toAcct, assetID):
+    '''
+    approve the asset to another address
+    :param toAcct:
+    :param assetID:
+    :return:
+    '''
     if checkToAcctAndAsset(toAcct, assetID) == False:
         return False
 
@@ -167,8 +215,8 @@ def approve(toAcct, assetID):
     if not assetOwner:
         return False
 
-    key = concatkey(concatkey(APPROVE_PREFIX, assetOwner), assetID)
-
+    # key = concatkey(concatkey(APPROVE_PREFIX, assetOwner), assetID)
+    key = concatkey(APPROVE_PREFIX, assetID)
     if not Get(ctx, key):
         Put(ctx, key, toAcct)
         Notify(['approve', assetOwner, toAcct, assetID])
@@ -178,12 +226,22 @@ def approve(toAcct, assetID):
 
 
 def getApproved(assetID):
-    assetOwner = ownerOf(assetID)
-    key = concatkey(concatkey(APPROVE_PREFIX, assetOwner), assetID)
+    '''
+    get the approved address of the asset
+    :param assetID:
+    :return:
+    '''
+    key = concatkey(APPROVE_PREFIX, assetID)
     return Get(ctx, key)
 
 
 def takeOwnership(toAcct, assetID):
+    '''
+    take the approved asset
+    :param toAcct:
+    :param assetID:
+    :return:
+    '''
     if CheckWitness(toAcct) == False:
         return False
 
@@ -191,14 +249,16 @@ def takeOwnership(toAcct, assetID):
     if not assetOwner:
         return False
 
-    key = concatkey(concatkey(APPROVE_PREFIX, assetOwner), assetID)
+    # key = concatkey(concatkey(APPROVE_PREFIX, assetOwner), assetID)
+    key = concatkey(APPROVE_PREFIX, assetID)
+
     approver = Get(ctx, key)
     if approver != toAcct:
         return False
 
     Delete(ctx, key)
     ownerKey = concatkey(OWNER_OF_ASSET_PREFIX, assetID)
-    Put(ctx, ownerKey, assetID)
+    Put(ctx, ownerKey, toAcct)
     increaseOwnerBalance(toAcct)
     Notify(['transfer', assetOwner, toAcct, assetID])
 
@@ -210,6 +270,12 @@ def concatkey(str1, str2):
 
 
 def checkToAcctAndAsset(toAcct, assetID):
+    '''
+    validation function
+    :param toAcct:
+    :param assetID:
+    :return:
+    '''
     if len(toAcct) != 20:
         return False
     owner = ownerOf(assetID)
@@ -221,17 +287,34 @@ def checkToAcctAndAsset(toAcct, assetID):
 
 
 def increaseOwnerBalance(acct):
+    '''
+    increase the owner balance
+    :param acct:
+    :return:
+    '''
     balance = balanceOf(acct)
     key = concatkey(OWNER_BALANCE_PREFIX, acct)
     Put(ctx, key, balance + 1)
 
 
 def tokenMetadata(assetID):
+    '''
+    this should be replaced by queryAssetByID
+    :param assetID:
+    :return:
+    '''
     #for example
     return concatkey("tokenMetadata", assetID)
 
 
 def createAsset(name, url, type):
+    '''
+    create a new asset
+    :param name:
+    :param url:
+    :param type:
+    :return:
+    '''
     timestamp = GetTime()
     totalcount = Get(ctx, TOTAL_ASSET_COUNT)
     Put(ctx, TOTAL_ASSET_COUNT, totalcount + 1)
@@ -244,6 +327,12 @@ def createAsset(name, url, type):
 
 
 def buyAsset(account, assetID):
+    '''
+    buy an asset by any user
+    :param account:
+    :param assetID:
+    :return:
+    '''
     if ownerOf(assetID):
         return False
     if CheckWitness(account) == False:
@@ -252,20 +341,36 @@ def buyAsset(account, assetID):
         key = concatkey(OWNER_OF_ASSET_PREFIX, assetID)
         Put(ctx, key, account)
         increaseOwnerBalance(account)
+        ontbalance = Get(ctx, ONT_BALANCE_PREFIX)
+        Put(ctx, ONT_BALANCE_PREFIX, ontbalance + 1)
         Notify(['transfer', '', account, assetID])
         return True
     return False
 
 
 def queryAssetIDByIndex(idx):
+    '''
+    query assetid by index
+    :param idx:
+    :return:
+    '''
     return Get(ctx, concatkey(ASSET_INDEX_PREFIX, idx))
 
 
 def queryAssetByID(assetID):
+    '''
+    query asset detail by assetID
+    :param assetID:
+    :return:
+    '''
     return Get(ctx, concatkey(ASSET_ID_PREFIX, assetID))
 
 
 def queryAssetCount():
+    '''
+    query total count of assets
+    :return:
+    '''
     return Get(ctx, TOTAL_ASSET_COUNT)
 
 
@@ -283,7 +388,6 @@ def transferONT(fromacct, toacct, amount):
     if CheckWitness(fromacct):
         param = state(fromacct, toacct, amount)
         res = Invoke(1, contractAddress, 'transfer', [param])
-        Notify(res)
 
         if res and res == b'\x01':
             return True
@@ -292,3 +396,41 @@ def transferONT(fromacct, toacct, amount):
 
     else:
         return False
+
+
+def transferONTFromContract(toacct, amount):
+    """
+    transfer ONT from contract
+    :param fromacct:
+    :param toacct:
+    :param amount:
+    :return:
+    """
+    # ONT native contract address
+    contractAddress = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01')
+
+    param = state(selfAddr, toacct, amount)
+    res = Invoke(1, contractAddress, 'transfer', [param])
+
+    if res and res == b'\x01':
+        return True
+    else:
+        return False
+
+
+def withdraw(account):
+    '''
+    withdraw ont to admin
+    :param account:
+    :return:
+    '''
+    if CheckWitness(account) == False:
+        return False
+    ontbalance = Get(ctx, ONT_BALANCE_PREFIX)
+    if ontbalance > 0:
+        if transferONTFromContract(account, ontbalance):
+            Delete(ctx, ONT_BALANCE_PREFIX)
+            return True
+        else:
+            return False
+    return False
